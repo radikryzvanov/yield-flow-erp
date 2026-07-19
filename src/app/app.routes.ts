07f1d03a8fe6e067c3fdfdf8f2940560deda2b30
Radik <radik.hanif@yandex.ru>
2026-07-19 22:02:03 +0400

    feat(routing): connect poultry module to main app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'poultry',
+    pathMatch: 'full'
+  },
+  {
+    path: 'poultry',
+    loadChildren: () => import('./features/poultry-management/poultry-management.routes').then(m => m.POULTRY_MANAGEMENT_ROUTES)
+  }
+];
