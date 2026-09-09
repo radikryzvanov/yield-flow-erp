6974f9c9baa9e68ff90eabebc805e80a3ed52e6a
Radik <radik.hanif@yandex.ru>
2026-09-09 23:38:17 +0400

    chore: remove dead spec and unused shared components

--- src/app/shared/directives/numeric-only.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NumericOnlyDirective } from './numeric-only.directive';
-
-describe('NumericOnlyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NumericOnlyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
